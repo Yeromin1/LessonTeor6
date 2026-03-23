0ee0b9ed46966e1fc0dae275114236b8aacb641f
Yeromin Dmytro <eremin.dmitriy@ukr.net>
2026-03-23 21:32:33 +0200

    StatusFilter

--- src/components/StatusFilter/StatusFilter.jsx
+++ src/components/StatusFilter/StatusFilter.jsx
@@ -1,15 +1,36 @@
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { Button } from "../Button/Button";
 import css from "./StatusFilter.module.css";
+import { setStatusFilter } from "../../redux/actions";
 
 export const StatusFilter = () => {
+  const dispatch = useDispatch();
   const filter = useSelector((state) => state.filters.status);
+
+  const handleFilterChange = (filter) => dispatch(setStatusFilter(filter));
 
   return (
     <div className={css.wrapper}>
-      <Button>All {filter === "all" && "is active"} </Button>
-      <Button>Active {filter === "active" && "is active"} </Button>
-      <Button>Completed {filter === "completed" && "is active"}</Button>
+      <Button
+        selected={filter === "all"}
+        onClick={() => handleFilterChange("all")}
+      >
+        All
+      </Button>
+
+      <Button
+        selected={filter === "active"}
+        onClick={() => handleFilterChange("active")}
+      >
+        Active {filter === "active" && "is active"}{" "}
+      </Button>
+
+      <Button
+        selected={filter === "completed"}
+        onClick={() => handleFilterChange("completed")}
+      >
+        Completed {filter === "completed" && "is active"}
+      </Button>
     </div>
   );
 };
--- src/components/Task/Task.jsx
+++ src/components/Task/Task.jsx
@@ -1,7 +1,7 @@
 import { MdClose } from "react-icons/md";
 import css from "./Task.module.css";
 import { useDispatch } from "react-redux";
-import { deleteTask } from "../../redux/actions";
+import { deleteTask, toggleCompleted } from "../../redux/actions";
 
 export const Task = ({ task }) => {
   const dispatch = useDispatch();
@@ -10,12 +10,17 @@ export const Task = ({ task }) => {
     dispatch(deleteTask(task.id));
   };
 
+  const handleToggle = () => {
+    dispatch(toggleCompleted(task.id));
+  };
+
   return (
     <div className={css.wrapper}>
       <input
         type="checkbox"
         className={css.checkbox}
         checked={task.completed}
+        onChange={handleToggle}
       />
 
       <p className={css.text}>{task.text}</p>
