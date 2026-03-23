3564a3052af090fd0ec58e2742db78ec43471e59
Yeromin Dmytro <eremin.dmitriy@ukr.net>
2026-03-23 21:14:42 +0200

    Task

--- src/components/Task/Task.jsx
+++ src/components/Task/Task.jsx
@@ -1,7 +1,15 @@
 import { MdClose } from "react-icons/md";
 import css from "./Task.module.css";
+import { useDispatch } from "react-redux";
+import { deleteTask } from "../../redux/actions";
 
 export const Task = ({ task }) => {
+  const dispatch = useDispatch();
+
+  const handleDelete = () => {
+    dispatch(deleteTask(task.id));
+  };
+
   return (
     <div className={css.wrapper}>
       <input
@@ -9,8 +17,10 @@ export const Task = ({ task }) => {
         className={css.checkbox}
         checked={task.completed}
       />
+
       <p className={css.text}>{task.text}</p>
-      <button className={css.btn}>
+
+      <button className={css.btn} onClick={handleDelete}>
         <MdClose size={24} />
       </button>
     </div>
